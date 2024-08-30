486cada9a6213b3f7b5b455f0ed28ee6a41f4a4f
michael-decidr <michael@decidr.ai>
2024-08-30 12:27:06 +1000

    Added extra relays

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,6 @@
 export const RELAYS = [
-    "wss://relay.ghostcopywrite.com"
+    "wss://relay.ghostcopywrite.com",
+    "wss://relay.snort.social",
+    "wss://relay.damus.io",
+    "wss://relay.primal.net"
   ]
