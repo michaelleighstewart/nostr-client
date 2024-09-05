7af0efc6b651f56eb43faf2836a101bb2e0a5bed
michael-decidr <michael@decidr.ai>
2024-09-05 21:59:13 +1000

    Refactor (3)

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -12,6 +12,7 @@ import { toast } from 'react-toastify';
 import { Link } from 'react-router-dom';
 import { motion, AnimatePresence } from 'framer-motion';
 import { getFollowers } from "../utils/profileUtils";
+import { fetchMetadataReactionsAndReplies } from '../utils/noteUtils';
 
 interface HomeProps {
   keyValue: string;
@@ -236,65 +237,7 @@ const Home : React.FC<HomeProps> = (props: HomeProps) => {
 
     useEffect(() => {
       if (!props.pool) return;
-
-      const fetchMetadataReactionsAndReplies = () => {
-          const pubkeysToFetch = new Set(events.map(event => event.pubkey));
-          const postsToFetch = events.map(event => event.id);
-
-          props.pool?.subscribeManyEose(
-              RELAYS,
-              [
-                  { kinds: [0], authors: Array.from(pubkeysToFetch) },
-                  { kinds: [7], '#e': postsToFetch },
-                  { kinds: [1], '#e': postsToFetch }
-              ],
-              {
-                  onevent(event: Event) {
-                      if (event.kind === 0) {
-                          const metadata = JSON.parse(event.content) as Metadata;
-                          setMetadata(cur => ({
-                              ...cur,
-                              [event.pubkey]: metadata
-                          }));
-                      } else if (event.kind === 7) {
-                          setReactions(cur => {
-                              const newReaction: Reaction = {
-                                  liker_pubkey: event.pubkey,
-                                  type: event.content,
-                                  sig: event.sig
-                              };
-                              const postId = event.tags.find(tag => tag[0] === 'e')?.[1];
-                              if (postId) {
-                                  const updatedReactions = { ...cur };
-                                  if (updatedReactions[postId]) {
-                                      if (!updatedReactions[postId].some(r => r.sig === newReaction.sig)) {
-                                          updatedReactions[postId] = [...updatedReactions[postId], newReaction];
-                                      }
-                                  } else {
-                                      updatedReactions[postId] = [newReaction];
-                                  }
-                                  return updatedReactions;
-                              }
-                              return cur;
-                          });
-                      } else if (event.kind === 1) {
-                          setReplies(cur => {
-                              const updatedReplies = { ...cur };
-                              const postId = event.tags.find(tag => tag[0] === 'e')?.[1];
-                              if (postId) {
-                                  updatedReplies[postId] = (updatedReplies[postId] || 0) + 1;
-                              }
-                              return updatedReplies;
-                          });
-                      }
-                  },
-                  onclose() {
-                  }
-              }
-          );
-      };
-
-      fetchMetadataReactionsAndReplies();
+      fetchMetadataReactionsAndReplies(props.pool, events, setMetadata, setReactions, setReplies);
   }, [events, props.pool]);
 
     async function sendMessage() {
