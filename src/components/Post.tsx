8198c19e1da76f5bf8e192831337d77b1a62299a
michaelleighstewart <michael.leigh.stewart@gmail.com>
2024-09-11 13:49:07 +1000

    Fix build

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { SimplePool, Event, getPublicKey, finalizeEvent } from 'nostr-tools';
+import { SimplePool, Event, finalizeEvent } from 'nostr-tools';
 import { useParams } from 'react-router-dom';
 import NoteCard from './NoteCard';
 import { RELAYS } from '../utils/constants';
